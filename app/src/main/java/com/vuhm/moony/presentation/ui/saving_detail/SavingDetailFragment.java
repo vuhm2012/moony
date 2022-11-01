dc37d1ede69537f1b14472b9bfc4c07599572487
minhvuhoang <hn.minhvu@gmail.com>
2022-11-01 19:47:02 +0700

    Update transaction detail

--- app/src/main/java/com/vuhm/moony/presentation/ui/saving_detail/SavingDetailFragment.java
+++ app/src/main/java/com/vuhm/moony/presentation/ui/saving_detail/SavingDetailFragment.java
@@ -3,9 +3,13 @@ package com.vuhm.moony.presentation.ui.saving_detail;
 import android.os.Bundle;
 
 import com.vuhm.moony.R;
+import com.vuhm.moony.databinding.FragmentSavingDetailBinding;
 import com.vuhm.moony.presentation.base.BaseFragment;
 
 public class SavingDetailFragment extends BaseFragment {
+
+    private FragmentSavingDetailBinding binding;
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_saving_detail;
@@ -13,11 +17,13 @@ public class SavingDetailFragment extends BaseFragment {
 
     @Override
     public void initControls(Bundle savedInstanceState) {
-
+        binding = (FragmentSavingDetailBinding) getBinding();
     }
 
     @Override
     public void initEvents() {
-
+        binding.btnBack.setOnClickListener(view -> {
+            pop(view);
+        });
     }
 }
--- app/src/main/java/com/vuhm/moony/presentation/ui/transaction_detail/TransactionDetailFragment.java
+++ app/src/main/java/com/vuhm/moony/presentation/ui/transaction_detail/TransactionDetailFragment.java
@@ -3,9 +3,13 @@ package com.vuhm.moony.presentation.ui.transaction_detail;
 import android.os.Bundle;
 
 import com.vuhm.moony.R;
+import com.vuhm.moony.databinding.FragmentTransactionDetailBinding;
 import com.vuhm.moony.presentation.base.BaseFragment;
 
 public class TransactionDetailFragment extends BaseFragment {
+
+    private FragmentTransactionDetailBinding binding;
+
     @Override
     public int getLayoutId() {
         return R.layout.fragment_transaction_detail;
@@ -13,11 +17,13 @@ public class TransactionDetailFragment extends BaseFragment {
 
     @Override
     public void initControls(Bundle savedInstanceState) {
-
+        binding = (FragmentTransactionDetailBinding) getBinding();
     }
 
     @Override
     public void initEvents() {
-
+        binding.btnBack.setOnClickListener(view -> {
+            pop(view);
+        });
     }
 }
